78230b4945ec5bbb30f3bf0897ebdb88a03f1b83
Sam <splavigne@gmail.com>
2014-02-17 16:09:16 -0500

    clean up and fix

--- magic_hardy.py
+++ magic_hardy.py
@@ -5,11 +5,10 @@ filename = sys.argv[1]
 temp = ""
 for line in open(filename):
     temp += line
-temp = temp.replace("\n", "")
-temp = temp.replace("\r", "")
-temp = temp.replace("\t", "")
 
-quotes = [l.strip() for l in temp.split(". ")]
+for t in ["\n", "\r", "\t"]:
+    temp = temp.replace(t, "")
+
 pat = re.compile(r'([A-Z][^\.!?]*[\.!?])', re.M)
 quotes = pat.findall(temp)
 
@@ -18,34 +17,11 @@ for line in sys.stdin:
     temp += line
 
 text = re.split('".+?"', temp)
-#text = temp.split('"')
-j = 0
+
+new_text = []
 for index, line in enumerate(text):
-    if index % 2 == 1 and j + 1 < len(quotes):
-        #text[index] = '"' + coming_insurrection[j] + '."' 
-        text[index] =  quotes[j]
-        j = j + 1
+    new_text.append(line)
+    if index + 1 < len(quotes):
+        new_text.append(quotes[index])
 
-print '"'.join(text)
-
-#import re
-#pattern = r'".+?"'
-#regex = re.compile(pattern)
-#for match in regex.finditer(temp):
-    #print "%s: %s" % (match.start(), match.group(0))
-
-#print '"'.join(text)
-
-#print "".join(text)
-#j = 0
-#for l in text:
-    #if i % 2 == 0:
-        #print l
-    #else:
-        #print '"' + coming_insurrection[j] + '."'
-        #j = j + 1
-    #i = i + 1
-
-#print text
-
-#print coming_insurrection
+print '"'.join(new_text)
